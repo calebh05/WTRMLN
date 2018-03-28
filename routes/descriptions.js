94a11d3f5b0afb0f49208496f594b2f495082152
Caleb <chess@2co.com>
2018-03-27 23:30:36 -0400

    dont break anything plz

--- routes/descriptions.js
+++ routes/descriptions.js
@@ -1,8 +1,9 @@
-var express = require("express");
-    router  = express.Router({mergeParams: true});
-    User    = require("../models/user");
-    Description    = require("../models/description");
-    middleware = require("../middleware");
+var express = require("express"),
+    router  = express.Router({mergeParams: true}),
+    User    = require("../models/user"),
+    Description    = require("../models/description"),
+    middleware = require("../middleware"),
+    logger  = require("../lib/logs.js");
 
 router.get("/descriptions", middleware.isLoggedIn, function(req, res) {
     res.render("descriptions");
@@ -11,14 +12,14 @@ router.get("/descriptions", middleware.isLoggedIn, function(req, res) {
 router.post("/descriptions,", middleware.isLoggedIn, function(req, res){
     User.findById(req.params.id, function(err, user){
         if(err){
-            console.log(err);
+            logger.error(err);
             res.redirect("/users");
         } else {
             Description.create(user, function(err, desc){
                 if(err){
-                    console.log(err);
+                    logger.error(err);
                 } else {
-                    console.log("Commentors username: " + req.user.username);
+                    logger.info("Commentors username: " + req.user.username);
                     desc.description.push(desc);
                     desc.save();
                     res.redirect("/users/" + user._id);
